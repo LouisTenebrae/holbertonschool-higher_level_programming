9bc7bb318ca0c5aaf3bf6f66ce8a81e84e16687b
LouisTenebrae <lme10112002@gmail.com>
2024-05-09 15:47:38 -0400

    2nd try, close enough

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
 if len(argv) == 1:
     print("0 arguments.")
 elif len(argv) > 1:
-    print("{} arguments:".format(len(argv)))
+    print("{} arguments:".format(len(argv) - 1))
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
