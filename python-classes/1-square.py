9f7b41d6954cf41b85dc9ae54c4b8bf406b4e707
LouisTenebrae <lme10112002@gmail.com>
2024-05-17 12:17:13 -0400

    1-6, with comments this time

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Creates class Square with private attribute size and validates size"""
 class Square:
+     """Represents a square"""
      def __init__(self, size=0):
+        """Initializes the data
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
         if type(size) != int:
             raise TypeError("size must be an integer")
