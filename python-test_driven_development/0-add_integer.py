ae643ff305431eca42aabc4ce3ffce43ed47e193
LouisTenebrae <lme10112002@gmail.com>
2024-05-20 14:24:04 -0400

    0-2

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,10 +2,14 @@
 """Defining function to add 2 int"""
 def add_integer(a, b=98):
     """Function to add 2 int"""
-    if type(a) is not int:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) is not int:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
     return a + b
 if __name__ == "__main__":
     import doctest
