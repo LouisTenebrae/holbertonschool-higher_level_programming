5a4d8e555a0000c5338c62bd04bd4d2052b5943c
LouisTenebrae <lme10112002@gmail.com>
2024-05-21 16:18:16 -0400

    Changes

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -73,6 +73,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Method that returns the biggest rectangle based on the area"""
         if not isinstance(rect_1, Rectangle):
