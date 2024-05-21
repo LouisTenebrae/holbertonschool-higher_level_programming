78676a58435b454770abe8c5aa65ebda6059bd36
LouisTenebrae <lme10112002@gmail.com>
2024-05-21 16:00:51 -0400

    6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,10 +4,13 @@
 
 class Rectangle:
     """Class that defines a rectangle"""
+    number_of_instances = 0
     def __init__(self, width=0, height=0):
         """__init__ method that sets the intance with a width and height"""
         self.height = height
         self.width = width
+
+        Rectangle.number_of_instances += 1
 
     @property
     def height(self):
@@ -62,13 +65,4 @@ class Rectangle:
     def __del__(self):
         """Method that prints a message when an instance is deleted"""
         print("Bye rectangle...")
-
-    def bigger_or_equal(rect_1, rect_2):
-        """Method that returns the biggest rectangle based on the area"""
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        return rect_2
+        Rectangle.number_of_instances -= 1
