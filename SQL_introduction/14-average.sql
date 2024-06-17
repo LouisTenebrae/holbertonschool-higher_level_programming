ab82415fb495b1de113220b29227d8eec5d40999
LouisTenebrae <lme10112002@gmail.com>
2024-06-17 19:21:07 -0400

    fix

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- Script that calculates the average score
-SELECT AVG(score) FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,5 @@
 -- Script that lists all records of the second_table table
-SELECT * FROM second_table;
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL AND name != ''
+ORDER BY score DESC;
