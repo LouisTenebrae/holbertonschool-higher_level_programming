20d3a39994cab25cc6caede1c04293ae6cc64f8c
LouisTenebrae <lme10112002@gmail.com>
2024-06-21 12:19:49 -0400

    ...

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
                                passwd=argv[2],
                                db=argv[3])
     curs = database.cursor()
-    curs.execute("SELECT cities.name FROM cities INNER JOIN states ON\
-                  cities.state_id=states.id WHERE states.name LIKE %s\
-                  ORDER BY cities.id ASC", (argv[4]))
+    curs.execute("SELECT cities.name FROM cities INNER JOIN states ON \
+                  cities.state_id=states.id WHERE states.name LIKE %s \
+                  ORDER BY cities.id ASC", [argv[4]])
     rows = curs.fetchall()
-    for row in rows:
-        print(row)
+    s = []
+    print(", ".join(row[0] for row in rows))
     curs.close()
     database.close()
